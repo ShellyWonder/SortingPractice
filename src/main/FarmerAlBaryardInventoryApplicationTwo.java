4e22a43f100e164f675766113dbd708d8d7af27f
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-06-29 07:18:09 -0500

    completed project

--- src/main/FarmerAlBaryardInventoryApplicationTwo.java
+++ src/main/FarmerAlBaryardInventoryApplicationTwo.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Arrays;
+
 import main.animals.Pig;
 import main.service.FileService;
 
@@ -9,16 +11,36 @@ public class FarmerAlBaryardInventoryApplicationTwo {
 		
 		FileService fileService = new FileService();
 		Pig[] pigs = fileService.getPigsFromFile();
-		String[] pigNames = new String[4];
+		String[] pigNames = new String[pigs.length];
   		
 		// SORT the pigs by name...
 		
 		// 1. run a for loop on the pig array
-		// 2. In the for loop create an array called pig names, get the names of the pigs using
-		// the getName getter from the Pig class, save it into the pigNames string array
+		int i = 0;
+		for(Pig pig: pigs) {
+			
+			try {
+				// 2. In the for loop create an array called pig names, get the names of the pigs using		
+				// the getName getter from the Pig class, save it into the pigNames string array
+				if (pig != null) {
+					pigNames[i] = pig.getName();
+					i++;
+				}
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Program cannot read the file.");
+				e.printStackTrace();
+			}
+			 
+		}	
+		pigNames = Arrays.copyOf(pigNames, i);
 		// 3. Sort the pig names
+		Arrays.sort(pigNames);	
 		// 4. Print the sorted names to the console
-
+		System.out.println(Arrays.toString(pigNames));
+		
 	}
-
 }
+
+
--- src/main/animals/Pig.java
+++ src/main/animals/Pig.java
@@ -4,14 +4,32 @@ package main.animals;
 public class Pig {
 
 	// CREATE the field "name" which is a String
+	private String name;
 	// CREATE the field "status" which is a String
-	
+	private String status;
+	public Pig() {
+	}
 	public Pig(String name, String status) {
-		// CREATE the constructor
+		this.name = name;
+		this.status = status;
 	}
 	
 	
-	// Create getter and setter for name and status
+	//getters and setters
+	public String getName() {
+		return name;
+	}	
+	public void setName(String name) {
+		this.name = name;
+	}			
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 	
 	
 
